66b8e7680336a9762240a3c732a9a278569ff437
fingoldin <vassilioskaxiras@gmail.com>
2017-03-18 04:24:59 -0700

    accurate

--- servo_test.ino
+++ servo_test.ino
@@ -1,68 +1,128 @@
 #include <Servo.h>
 
+#define E  2.7182818284590452353602874713527f
+
 const int motorpin = 5;
 Servo motor;
- 
-const int motorPin1 = 6;
-const int motorPin2 = 5;
-const int speedPin = 10;
+
+const int buttonpin = 2;
 
 const int sensorPin = A0;
 
 // All dimensions in centimeters
-const double diam = 5.5;
-const double desiredDist = 100.0;
-const double stoppingDelay = 1;
+const double diam = 7.87404;
+const double desiredDist = 200.0;
+
+const double stopBefore = 2.0; // distance
+
+const int stopMVel = 91;
+const int startMVel = 100;
+const int maxMVel = 114;
+
+const int lowerThres = 50;
+
+const double firstOffset = 60.0;
+const double secondOffset = 60.0;
 
 void setup()
 {
+  pinMode(buttonpin, INPUT);
+  
   motor.attach(motorpin);
 
   Serial.begin(9600);
+  
+  motor.write(stopMVel);
+}
 
-  // 91 is stop
-  motor.write(120);
+int getVel(double x)
+{
+  /*double a = 0.5 * (double)desiredDist;
+  double c = 100.0;
+  double p = (x-a) * (x-a) / c;
+  
+  double v = (double)(maxMVel - stopMVel) / pow(E, p);
+
+  return (stopMVel + (int)v);*/
+
+  if(x < firstOffset)
+    return (double)(maxMVel - startMVel) * (x / firstOffset) + startMVel;
+  else if(x > desiredDist - secondOffset)
+    return (double)(maxMVel - stopMVel) * ((desiredDist - x) / secondOffset) + stopMVel;
+  else
+    return maxMVel;
 }
  
-const int lowerThres = 100;
 int count = 0;
 int pR = lowerThres + 1;
- 
- 
-int stopped = 0;
-long time_prev;
-double running_speed = 0;
- 
+
+bool stopped = true;
+
+int pb = -1;
+
+long starttime = -1;
+
+long timeout = 8000;
+
 void loop()
 { 
+  int b = digitalRead(buttonpin);
+
+  if(millis() - starttime > timeout && starttime != -1)
+  {
+    stopped = true;
+    starttime = millis();
+  }
+  else if(b != pb && pb != -1)
+  {
+    //Serial.println("button");
+
+    delay(800);
+    
+    stopped = !stopped;
+
+    if(!stopped)
+    {
+      starttime = millis();
+    }
+  }
+
+  pb = b;
+
   if(stopped)
+  {
+    motor.write(stopMVel);
     return;
- 
+  }
+  
   int r = analogRead(sensorPin);
+
+  //Serial.println(r);
  
   if(r > lowerThres && pR < lowerThres)
-  {
-    double speed = (PI * diam / 4.0) / double(time_prev-millis());
-    running_speed = (running_speed + speed)/2.0;
     count++;
-    time_prev = millis();
-  }
-  Serial.println(r);
-  //Serial.println(running_speed * 1000);
- 
+  
   pR = r;
- 
-  if(((double)(count + 1)/ 4.0f) * PI * diam > desiredDist - stoppingDelay * running_speed)
-  //(count + 1 >= 200)
-  {
-    //digitalWrite(motorPin1, LOW);
-    //digitalWrite(motorPin2, LOW);
-    //analogWrite(speedPin, 0);
 
-    motor.write(91);
+  double d = ((double)(count) / 2.0) * PI * diam;
+
+  int v = getVel(d);
+
+  /*Serial.print(d);
+  Serial.print("   ");
+  Serial.println(v);*/
+
+  //Serial.println(digitalRead(buttonpin));
  
-    stopped = 1;
+  if(d > (desiredDist - stopBefore))
+  {
+    motor.write(stopMVel);
+
+    count = 0;
+ 
+    stopped = true;
   }
- 
+  else
+    motor.write(v);
 }
 
